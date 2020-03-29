e997467aa8b569ac089c3b248a0be57832cd5f1c
xiaozhengyu <zhengyupython@qq.com>
2020-03-29 20:07:47 +0800

    递归题目

--- Java/代码/src/main/java/com/learn/java/Main.java
+++ Java/代码/src/main/java/com/learn/java/Main.java
@@ -1,6 +1,8 @@
 package com.learn.java;
 
 
+import java.util.Scanner;
+
 /**
  * @author xzy
  * @date 2020-03-25 18:56
@@ -9,20 +11,12 @@ package com.learn.java;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Main.jumpFloor(1));
-        System.out.println(Main.jumpFloor(2));
-        System.out.println(Main.jumpFloor(3));
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
     }
 
-    public static int jumpFloor(int target) {
-        if (target == 1) {
-            //1. 还剩一个台阶，只有一种选择。
-            return 1;
-        } else if (target == 2) {
-            //2. 还剩两个台阶，有两种选择。
-            return 2;
-        } else {
-            return jumpFloor(target - 1) + jumpFloor(target - 2);
-        }
+    public int JumpFloorII(int target) {
+        return (int) Math.pow(2, target - 1);
     }
 }
