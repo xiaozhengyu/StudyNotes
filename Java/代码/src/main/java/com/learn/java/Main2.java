981c296b9e76f7b088b26b6e102e3bfa1b6dbcff
xiaozhengyu <zhengyupython@qq.com>
2020-04-19 20:44:11 +0800

    ListIterator接口

--- Java/代码/src/main/java/com/learn/java/Main2.java
+++ Java/代码/src/main/java/com/learn/java/Main2.java
@@ -9,5 +9,15 @@ import java.util.*;
  */
 public class Main2 {
     public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<>();
+        integers.add(1);
+        integers.add(2);
+        integers.add(3);
+
+        ListIterator<Integer> iterator = integers.listIterator();
+        System.out.print(iterator.next());
+        iterator.add(4);//OK
+        iterator.set(5);//ERROR - java.lang.IllegalStateException
+        iterator.remove();//ERROR - java.lang.IllegalStateException
     }
 }
