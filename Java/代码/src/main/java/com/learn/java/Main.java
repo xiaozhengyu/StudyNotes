9636e9f4a7e5005bdcc7b988ebcd31ca249a0134
xiaozhengyu <zhengyupython@qq.com>
2020-05-04 19:54:03 +0800

    逻辑结构物理结构

--- Java/代码/src/main/java/com/learn/java/Main.java
+++ Java/代码/src/main/java/com/learn/java/Main.java
@@ -1,5 +1,7 @@
 package com.learn.java;
 
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author xzy
@@ -9,5 +11,49 @@ package com.learn.java;
 
 public class Main {
     public static void main(String[] args) {
+        List<UserIp> userIpList = new ArrayList<>();
+        userIpList.add(new UserIp("a", "ip1"));
+        userIpList.add(new UserIp("a", "ip2"));
+        userIpList.add(new UserIp("b", "ip3"));
+        userIpList.add(new UserIp("c", "ip4"));
+        userIpList.add(new UserIp("d", "ip5"));
+        Map<String, String> user2ip = userIpList
+                .stream()
+                .collect(Collectors.toMap(
+                        UserIp::getUserId,
+                        UserIp::getIpAddress,
+                        (a, b) -> a + ";" + b));
+        Map<String, List<UserIp>> user2ips = userIpList
+                .stream()
+                .collect(Collectors.groupingBy(UserIp::getUserId));
+    }
+}
+
+class UserIp {
+    private String userId;
+    private String ipAddress;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public UserIp(String userId, String ipAddress) {
+        this.userId = userId;
+        this.ipAddress = ipAddress;
+    }
+
+    public UserIp() {
     }
 }
