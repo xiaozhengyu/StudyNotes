78547a6540c57070353713ef6a3cc645326cf2eb
xiaozhengyu <zhengyupython@qq.com>
2020-07-13 08:12:48 +0800

    局部变量表、操作数栈

--- Java/代码/src/main/java/com/learn/java/Main2.java
+++ Java/代码/src/main/java/com/learn/java/Main2.java
@@ -1,29 +1,38 @@
 package com.learn.java;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * @author xzy
  * @date 2020-04-06 20:53
  */
 public class Main2 {
+    static class Human{
+        public void say(){
+            System.out.println("Human");
+        }
+    }
+    static class Man extends Human{
+        @Override
+        public void say() {
+            System.out.println("Man");
+        }
+    }
+    static class Woman extends Human{
+        @Override
+        public void say() {
+            System.out.println("Woman");
+        }
+    }
+
     public static void main(String[] args) {
-        Set<String> a = new HashSet<>();
-        Set<String> b = new HashSet<>();
+        Human human;
 
-//        a.add("张三");
-//        a.add("李四");
-//        a.add("王五");
+        human = new Human();
+        human.say();
 
-        b.add("张三");
-        b.add("张飞");
-        b.add("刘备");
+        human = new Man();
+        human.say();
 
-        Set<String> c = new HashSet<>(a);
-        Set<String> d = new HashSet<>(b);
-
-        c.removeAll(b);
-        d.removeAll(a);
+        human = new Woman();
+        human.say();
     }
 }
