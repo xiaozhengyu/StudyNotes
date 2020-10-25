b790860051ca3da468eb39cf50a47c038b4cf07e
xiaozhengyu <zhengyupython@qq.com>
2020-10-25 15:08:31 +0800

    字符串对象 & 堆 & 常量池

--- Java/代码/src/main/java/com/learn/java/Main.java
+++ Java/代码/src/main/java/com/learn/java/Main.java
@@ -1,14 +1,13 @@
 package com.learn.java;
 
-import com.learn.ClassA;
-
 /**
  * @author xzy
  * @date 2020-03-25 18:56
  * 说明：测试
  */
 public class Main {
-
     public static void main(String[] args) {
+        String s4 = new String("1024");
+        System.out.println(s4);
     }
 }
