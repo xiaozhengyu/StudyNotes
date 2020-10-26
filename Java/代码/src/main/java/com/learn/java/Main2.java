4fbbdce477744a652d002a63cadccb577244d3f3
xiaozhengyu <zhengyupython@qq.com>
2020-10-26 09:44:07 +0800

    Java运算符优先级

--- Java/代码/src/main/java/com/learn/java/Main2.java
+++ Java/代码/src/main/java/com/learn/java/Main2.java
@@ -1,12 +1,10 @@
 package com.learn.java;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
 /**
  * @author xzy
  * @date 2020-04-06 20:53
  */
 public class Main2 {
-
+    public static void main(String[] args) {
+    }
 }
