9e48838014962998766b02312fc732fd657170d7
xiaozhengyu <zhengyupython@qq.com>
2020-12-07 17:28:28 +0800

    Map接口源码阅读

--- Java/代码/src/main/java/com/learn/java/Main.java
+++ Java/代码/src/main/java/com/learn/java/Main.java
@@ -1,14 +1,11 @@
 package com.learn.java;
 
-import java.util.Map;
-
 /**
  * @author xzy
  * @date 2020-03-25 18:56
  * 说明：测试
  */
 public abstract class Main {
-    public static void main(String[] args) throws Exception {
-        Map
+    public static void main(String[] args) {
     }
 }
