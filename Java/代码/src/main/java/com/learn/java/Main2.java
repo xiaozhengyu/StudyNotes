b6b7db9bd02a8480b366c97698042380b362ae68
xiaozhengyu <zhengyupython@qq.com>
2021-02-08 23:23:42 +0800

    组件注册

--- Java/代码/src/main/java/com/learn/java/Main2.java
+++ Java/代码/src/main/java/com/learn/java/Main2.java
@@ -6,12 +6,6 @@ package com.learn.java;
  */
 public class Main2 {
     public static void main(String[] args) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder
-                .append("aaa").append(",")
-                .append("bbb").append(",")
-                .append("ccc").append(",");
-        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
-        System.out.println(stringBuilder.toString());
+
     }
 }
