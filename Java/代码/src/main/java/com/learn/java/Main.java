f19a59f2765614f84f840d562ec0bd999fe7d80d
xiaozhengyu <zhengyupython@qq.com>
2021-02-18 14:06:54 +0800

    Spring IOC

--- Java/代码/src/main/java/com/learn/java/Main.java
+++ Java/代码/src/main/java/com/learn/java/Main.java
@@ -2,6 +2,9 @@ package com.learn.java;
 
 import com.learn.java.annotation.MyAnnotation;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @author xzy
  * @date 2020-03-25 18:56
@@ -10,8 +13,6 @@ import com.learn.java.annotation.MyAnnotation;
 @MyAnnotation
 public class Main {
     public static void main(String[] args) throws Exception {
-        // 0x7fffffff = 0111 1111 1111 1111 1111 1111 1111 1111
-        char[] chars1 = new char[Integer.MAX_VALUE];
-        char[] chars2 = new char[Integer.MAX_VALUE+1];
+
     }
 }
